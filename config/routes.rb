b1288ab2a456341c01ea9f420182a7bed75339a3
denisdbell <denisdbell@egmail.com>
2017-08-30 22:27:19 -0500

    Add custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get "red/:url" , to: "redirects#red"
+
   resources :redirects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
