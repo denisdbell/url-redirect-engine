2a9e5bd87bff195ba471aeea4e91f92f0c8ea0ea
denisdbell <denisdbell@egmail.com>
2017-08-31 23:45:44 -0500

    Add   skip_before_action :verify_authenticity_token to remove AuthenticityToken validation

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,5 +1,7 @@
 class RedirectsController < ApplicationController
   before_action :set_redirect, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+
 
   # GET /redirects
   # GET /redirects.json
